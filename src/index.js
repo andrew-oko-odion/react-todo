4785ce7846e76955739506aa7ecbb78cdee7b5a1
andrew oko-odion <mrniceguy4net@yahoo.com>
2017-06-24 22:27:32 +0100

    Remove CDN and add it locally

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from 'components/app';
+import React from "react";
+import { render } from "react-dom";
+import App from "components/app";
 
-render(<App />, document.getElementById('app'));
+render(<App />, document.getElementById("app"));
